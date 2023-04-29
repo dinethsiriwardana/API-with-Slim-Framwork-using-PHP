2bce4aab2ddb3c026eb136a35f216614d0176c42
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2023-04-30 02:31:23 +0530

    Add User

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -14,7 +14,21 @@ class UserController
     {
 
         $pdo = (new Database())->pdo;
-        $sth = $pdo->prepare("SELECT * FROM user");
+        $sth = $pdo->prepare("SELECT userid,name,email FROM user");
+        $sth->execute();
+
+        $results = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+        $payload = json_encode($results);
+        // $response->getBody()->write($payload);
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    public function byuser(ServerRequestInterface $request, ResponseInterface $response, $args)
+    {
+
+        $pdo = (new Database())->pdo;
+        $sth = $pdo->prepare("SELECT userid,name,email FROM user WHERE userid = " . $args['id']);
         $sth->execute();
 
         $results = $sth->fetchAll(PDO::FETCH_ASSOC);
@@ -24,17 +38,31 @@ class UserController
         return $response->withHeader('Content-Type', 'application/json');
     }
 
-    public function byuser(ServerRequestInterface $request, ResponseInterface $response, $args)
+    public function adduser(ServerRequestInterface $request, ResponseInterface $response, $args)
     {
 
+        // Get the request body as JSON
+        $requestBody = $request->getBody();
+        $data = json_decode($requestBody, true);
+
+        // Insert the new user into the database
+        // $sql = "INSERT INTO user (name, email, password) VALUES ('" . $data['name'] . "', '" . $data['email'] . "', '" . $data['password'] . "')";
+        // echo $sql;
         $pdo = (new Database())->pdo;
-        $sth = $pdo->prepare("SELECT * FROM user WHERE userid = " . $args['id']);
+        $sql = "INSERT INTO user (name, email, password) VALUES (:name, :email, :password)";
+        $sth = $pdo->prepare($sql);
+        $sth->bindParam(':name', $data['name'], PDO::PARAM_STR);
+        $sth->bindParam(':email', $data['email'], PDO::PARAM_STR);
+        $sth->bindParam(':password', $data['password'], PDO::PARAM_STR);
         $sth->execute();
+        // Get the ID of the new user
+        $newUserId = $pdo->lastInsertId();
 
-        $results = $sth->fetchAll(PDO::FETCH_ASSOC);
-
-        $payload = json_encode($results);
-        $response->getBody()->write($payload);
-        return $response->withHeader('Content-Type', 'application/json');
+        // Create a response with the new user's ID
+        $responseData = array('id' => $newUserId);
+        $responseBody = json_encode($responseData);
+        $response->getBody()->write($responseBody);
+        $response = $response->withStatus(201)->withHeader('Content-Type', 'application/json');
+        return $response;
     }
 }
--- routes/routes.php
+++ routes/routes.php
@@ -7,3 +7,4 @@ require __DIR__ . '/../Controller/UserController.php';
 
 $app->get('/users', [\UserController::class, 'showall']);
 $app->get('/users/{id}', [\UserController::class, 'byuser']);
+$app->post('/users/adduser', [\UserController::class, 'adduser']);
